e4d3057c2e5a90228bb69b29f2c0b810dbfb2376
Richard Kovacs <dev.kovacs.richard90@gmail.com>
2023-05-10 19:21:41 +0200

    git flow representation

--- git_tutorial.py
+++ git_tutorial.py
@@ -10,4 +10,18 @@ Git kezelté akarom tenni a fejlesztésem:
     parancs kiadása abban a mappában, amit git kezelté akarok tenni
 
     létrejön egy 'master' nevű branch -> a kódom 1 adott állapota
+
+
+A fejlesztésem során a file-jaim / mappáimnak az következő státuszai vannak.
+ - untracked - még nem git kezelt
+
+ változás állapotai:
+  - not staged állapot - a változás még nincs a stageld változásokhoz adva
+  - staged állapot - ezek a változások már készen állnak a véglegesítésre, de bármikor módosítható
+  - committed - ez 1 "véglegesített, időbéjeggel ellátott" állapot -> erre az adott commit állapotra
+  bármikor vissza tudom állítani a fejlesztésem állapotát
+
+
+
+
 """
