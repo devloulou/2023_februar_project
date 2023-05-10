5f4c457d0cbfe55b6e3039d2bbb0a422ba2ccab4
Richard Kovacs <dev.kovacs.richard90@gmail.com>
2023-05-10 19:17:35 +0200

    test commit

---
+++ git_tutorial.py
@@ -0,0 +1,13 @@
+"""
+GIT - verziókezelő rendszer
+
+Github - Microsoft
+GitLab - 
+Bitbucket - Atlassian
+
+Git kezelté akarom tenni a fejlesztésem:
+1. lépés: git init 
+    parancs kiadása abban a mappában, amit git kezelté akarok tenni
+
+    létrejön egy 'master' nevű branch -> a kódom 1 adott állapota
+"""
