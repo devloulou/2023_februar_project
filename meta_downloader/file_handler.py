785bd7422bd5cdca320d092bc9ed7bd1e5894bf7
Richard Kovacs <dev.kovacs.richard90@gmail.com>
2023-05-31 18:28:58 +0200

    implement create_folder function

--- meta_downloader/file_handler.py
+++ meta_downloader/file_handler.py
@@ -9,9 +9,12 @@ class FileHandler:
     # def __init__(self, folder_path):
     #     self.folder_path = folder_path
     #     self.movies = self.get_files_path_from_folder()
+    root_folder = os.path.dirname(os.path.dirname(__file__))
+    meta_data = os.path.join(root_folder, 'meta_data')
+    posters = os.path.join(root_folder, 'posters')
 
     def __init__(self):
-        ...
+        self.create_folders()
 
     @staticmethod
     def get_files_path_from_folder(folder_path):
@@ -30,6 +33,13 @@ class FileHandler:
         with open(json_path, 'w', encoding="utf-8") as f:
             json.dump(data, f, ensure_ascii=False, indent=4)
 
+    def create_folders(self):
+        if not os.path.exists(self.meta_data):
+            os.mkdir(self.meta_data)
+        
+        if not os.path.exists(self.posters):
+            os.mkdir(self.posters)
+
 if __name__ == '__main__':
     f_path = r"C:\WORK\2023_februar_projekt\movies"
     test = FileHandler()
@@ -41,4 +51,4 @@ if __name__ == '__main__':
 
     # get_files_path_from_folder() függvény return valueját majd vizsgálnom kell, hogy üres e
 
-    test.write_json('valami/test.json', {'kulcs': 'ertek'})
+    #test.write_json('valami/test.json', {'kulcs': 'ertek'})
